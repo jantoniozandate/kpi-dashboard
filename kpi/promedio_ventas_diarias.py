22cace64fddd2de87404205aa04af68bfe13d60c
MigDev <angel_avil31@hotmail.com>
2025-05-07 18:45:03 -0600

    Promedio Ventas Diarias

--- kpi/promedio_ventas_diarias.py
+++ kpi/promedio_ventas_diarias.py
@@ -1,8 +1,17 @@
 # promedio_ventas_diarias.py
+import pandas as pd
 
 class PromedioVentasDiarias:
     def execute(self, df):
+
+        df['fecha'] = pd.to_datetime(df['fecha'])  # Asegura que sea tipo fecha
+        total_ventas = df["total"].sum()
+        numero_dias = df["fecha"].dt.date.nunique()  # número de días distintos
+        return round(total_ventas / numero_dias, 2)
+
+
         """
         Calcula el promedio de ventas diarias.
         Para simplificar: promedio = total de ventas / número de días.
         """
+
