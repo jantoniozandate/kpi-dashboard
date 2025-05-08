1be076b4752980e29c597864575b61895f54d310
Leyra Garcia <garciariosleyra@gmail.om>
2025-05-07 18:41:47 -0600

    venta por canal

--- kpi/clientes_nuevos.py
+++ kpi/clientes_nuevos.py
@@ -6,3 +6,14 @@ class CalcularRetencionClientes:
         Regresa los clientes nuevos.
         Para simplificar: nuevos = (# clientes que compraron por primera vez)
         """
+
+   if "clientes_id" not in df.columns:
+        return "El DataFrame no contiene la columna clientes_id"
+
+    frecuencia_clientes = df["clientes_id "].value_counts()
+
+    clientes_nuevos = (frecuencia_clientes == 1).sum()
+
+   if frecuencia_clientes.empty:
+       return"No hhay clientes en el archivo"
+       return f"Numero de clientes nuevos [Clientes_nuevos]"
